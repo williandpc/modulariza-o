c76823ec9e1a9a081987032ae76697989dcfc10c
willian <williancostajr@hotmail.com>
2023-03-24 03:37:56 -0300

    Curso em video

--- Ex da Aula 22.py
+++ Ex da Aula 22.py
@@ -1,19 +1,6 @@
-def fatorial(n):
-    f = 1
-    for c in range(n, 0 , -1):
-        f *= c
-    return f
-
-
-def dobro(n):
-    return n * 2
-
-
-def triplo(n):
-    return n * 3
-
+from uteis import numeros
 
 num = int(input('Digite um valor: '))
-fat = fatorial(num)
+fat = numeros.fatorial(num)
 print(f'O fatorial de {num} é {fat}')
-print(F'O dobro de {num} é {dobro(num)}')
+print(F'O dobro de {num} é {numeros.dobro(num)}')
